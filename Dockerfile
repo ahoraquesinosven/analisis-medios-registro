3d52150a5956b45117e82384600c71b2226f9d98
Gisela Morinigo <gisemo@gmail.com>
2025-05-13 23:41:13 -0300

    Testing parsing url

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.9
 
 WORKDIR /usr/src/app
 
-RUN pip install Jinja2
+RUN pip install tldextract
 RUN apt-get clean && apt-get update && apt-get install -y locales locales-all
--- analisis.py
+++ analisis.py
@@ -1,30 +1,40 @@
-#!/usr/bin/env python3
-import locale
-locale.setlocale(locale.LC_TIME, 'es_AR.utf8')
+from urllib.parse import urlparse, urlunparse, quote, unquote, parse_qs
+import tldextract
 
-import datetime as dt
-from jinja2 import Template
-# cargar CSV > array de diccionario con el formato que corresponde Date, integer, etc
-def load_cases():
-    return [
-        { "fecha": dt.datetime(2020,1,20) },
-        { "fecha":dt.datetime(2020,3,28) }
-    ]
+# Parse a URL
+url = "https://otrasvocesmdz.com.ar/brutal-intento-de-femicidio-en-el-palomar-apunalo-a-su-ex-pareja-y-huyo/"
+parsed_url = urlparse(url)
+print("Netloc:", parsed_url.netloc)
 
-# agarro cada diccionario > render de un template libreria python jinja2  > array de strings
-template = Template('Hello {{ fecha.strftime("%d de %B de %Y") }}!')
-
-def render_template(case):
-    return template.render(case)
-
-def render_obituaries(data):
-    obituaries = map(render_template, data)
-
-    return obituaries
+extracted_info = tldextract.extract(parsed_url.netloc)
+print("Extracted info: ",extracted_info)
 
 
+print("Scheme:", parsed_url.scheme)
+print("Netloc:", parsed_url.netloc) # lo que necesito
+print("Path:", parsed_url.path)
+# print("Params:", parsed_url.params)
+# print("Query:", parsed_url.query)
+# print("Fragment:", parsed_url.fragment)
 
-# texto plano probar
-#TODO
+def get_url_netloc(url):
+    parsed_url = urlparse(url)
+    return parsed_url.netloc
 
-print(list(render_obituaries(load_cases())))
+
+
+
+
+
+# # Encode a query string
+# query_string = "data with spaces and & characters"
+# encoded_query = quote(query_string)
+# print("Encoded query:", encoded_query)
+
+# # Decode a query string
+# decoded_query = unquote(encoded_query)
+# print("Decoded query:", decoded_query)
+
+# # Parse query parameters
+# query_params = parse_qs(parsed_url.query)
+# print("Query parameters:", query_params)
